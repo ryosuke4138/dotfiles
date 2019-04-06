51241a538eb96a8d79739070e60e02767ea7e882
Ryosuke Okamae <okamae.ryosuke@unipro.jp>
2019-04-06 11:11:44 +0900

    add sh

---
+++ dotfiles_links.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/ctags ~/.ctags
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/irbrc ~/.irbrc
+ln -sf ~/dotfiles/jshintrc  ~/.jshintrc
+ln -sf ~/dotfiles/nvim/common.vim ~/.config/nvim/common.vim
+ln -sf ~/dotfiles/nvim/functions.vim ~/.config/nvim/functions.vim
+ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/nvim/languages.vim ~/.config/nvim/languages.vim
+ln -sf ~/dotfiles/nvim/mappings.vim ~/.config/nvim/mappings.vim
+ln -sf ~/dotfiles/nvim/plugin_conf.vim ~/.config/nvim/plugin_conf.vim
+ln -sf ~/dotfiles/nvim/plugins.vim ~/.config/nvim/plugins.vim
